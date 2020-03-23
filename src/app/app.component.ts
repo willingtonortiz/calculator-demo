805757bf885d869e5848ebce4e8b7d52318c45cf
Willington <willingtonortizmaurtua@hotmail.com>
2020-03-23 15:18:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'githubpages-test';
+	title = 'Calculator-app';
+
+	public num1: number;
+	public num2: number;
+	public result: number;
+
+	add() {
+		this.result = this.num1 + this.num2;
+	}
+	substract() {
+		this.result = this.num1 - this.num2;
+	}
+	multiply() {
+		this.result = this.num1 * this.num2;
+	}
+	divide() {
+		this.result = this.num1 / this.num2;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
+	declarations: [AppComponent],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
